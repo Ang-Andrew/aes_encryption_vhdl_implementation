580ed2726a1fa9ac21d6c2537a9a3bdc6a4608f5
Adminstrator <admin@example.com>
2018-07-18 22:09:04 +1000

    added clock process for g_function

--- vivado_project/aes_encryption_implementation/aes_encryption_implementation.srcs/sources_1/imports/source/g_function.vhd
+++ vivado_project/aes_encryption_implementation/aes_encryption_implementation.srcs/sources_1/imports/source/g_function.vhd
@@ -16,6 +16,7 @@ use work.aes_encryption_lib.all;
 
 entity g_function is
   port (
+    clock      : in std_logic;
     i_rcon_sel : in  integer;
     i_word     : in  Word;
     i_word2    : in  Word;
@@ -28,7 +29,11 @@ architecture Behavioral of g_function is
   signal temp_word      : Word;
   signal sub_temp_word  : Word;
   signal rcon_temp_word : Word;
-
+  signal rcon_sel_reg   : integer;    
+  signal word_reg       : Word;
+  signal word2_reg      : Word;
+  signal o_word_reg     : Word;
+    
 begin
 
   rotation : temp_word <= (i_word(1), i_word(2), i_word(3), i_word(0));
@@ -36,15 +41,26 @@ begin
   s_box_sub_0 : for i in 0 to 3 generate
     s_box_inst : entity work.aes_encryption_sbox
       port map(
+        clock  => clock,
         i_byte => temp_word(i),
         o_byte => sub_temp_word(i)
         );
   end generate;
 
-  rcon_addition : rcon_temp_word <= (sub_temp_word(0) xor Rcon(i_rcon_sel),
+  rcon_addition : rcon_temp_word <= (sub_temp_word(0) xor Rcon(rcon_sel_reg),
                                      sub_temp_word(1), sub_temp_word(2), sub_temp_word(3));
 
-  word2_addition : o_word <= (rcon_temp_word(0) xor i_word2(0), rcon_temp_word(1) xor i_word2(1),
-                              rcon_temp_word(2) xor i_word2(2), rcon_temp_word(3) xor i_word2(3));
+  word2_addition : o_word_reg <= (rcon_temp_word(0) xor word2_reg(0), rcon_temp_word(1) xor word2_reg(1),
+                              rcon_temp_word(2) xor word2_reg(2), rcon_temp_word(3) xor word2_reg(3));
+                              
+  process(clock)
+  begin
+    if rising_edge(clock) then
+        rcon_sel_reg <= i_rcon_sel;
+        word_reg     <= i_word;
+        word2_reg    <= i_word2;
+        o_word       <= o_word_reg;
+    end if;
+  end process;
 
 end Behavioral;
