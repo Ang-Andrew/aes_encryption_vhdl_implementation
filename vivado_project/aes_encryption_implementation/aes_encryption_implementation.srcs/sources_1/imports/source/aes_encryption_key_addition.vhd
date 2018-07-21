e155928670a82957a64764a5c46480232dc056e9
Adminstrator <admin@example.com>
2018-07-21 18:06:28 +1000

    modified aes_encryption_key_addition to have a clocked output

--- vivado_project/aes_encryption_implementation/aes_encryption_implementation.srcs/sources_1/imports/source/aes_encryption_key_addition.vhd
+++ vivado_project/aes_encryption_implementation/aes_encryption_implementation.srcs/sources_1/imports/source/aes_encryption_key_addition.vhd
@@ -25,18 +25,28 @@ entity aes_encryption_key_addition is
 end aes_encryption_key_addition;
 
 architecture Behavioral of aes_encryption_key_addition is
+    
+    signal i_state_reg      : State;
+    signal i_key_reg        : Key;
+    signal o_state_reg      : State;
+    
 
 begin
-
+    
+    key_addition:
+    for i in 0 to 3 generate
+        o_state_reg(i) <= (i_state_reg(i)(0) xor i_key_reg(i)(0),
+                           i_state_reg(i)(1) xor i_key_reg(i)(1),
+                           i_state_reg(i)(2) xor i_key_reg(i)(2),
+                           i_state_reg(i)(3) xor i_key_reg(i)(3));
+    end generate;
+    
     process(clock)
     begin
         if rising_edge(clock) then
-            for i in 0 to 3 loop
-                o_state(i) <= (i_state(i)(0) xor i_key(i)(0),
-                               i_state(i)(1) xor i_key(i)(1),
-                               i_state(i)(2) xor i_key(i)(2),
-                               i_state(i)(3) xor i_key(i)(3));
-            end loop;
+            i_state_reg <= i_state;
+            i_key_reg   <= i_key;
+            o_state     <= o_state_reg;            
         end if;
     end process;
 
