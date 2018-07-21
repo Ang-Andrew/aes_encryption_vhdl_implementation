fafa5ba405b15631e6a6976842a7cdb1ac247013
Adminstrator <admin@example.com>
2018-07-21 17:47:05 +1000

    modified aes_encryption_key_schedule testbench for a clock process

--- vivado_project/aes_encryption_implementation/aes_encryption_implementation.srcs/sim_1/new/aes_encryption_key_schedule_tb.vhd
+++ vivado_project/aes_encryption_implementation/aes_encryption_implementation.srcs/sim_1/new/aes_encryption_key_schedule_tb.vhd
@@ -21,6 +21,7 @@ architecture Behavioral of aes_encryption_key_schedule_tb is
     
     component aes_encryption_key_schedule is
     Port ( 
+        clock                   : in std_logic;
         i_key                   : in Key;
         o_key_schedule          : out Key_schedule                           
     );
@@ -28,11 +29,14 @@ architecture Behavioral of aes_encryption_key_schedule_tb is
     
     signal tb_key               : Key;
     signal tb_key_schedule      : Key_schedule;
+    signal tb_clock             : std_logic;
+    constant clk_period         : time := 10 ns;
 
 
 begin
     
     uut: aes_encryption_key_schedule port map(
+        clock   => tb_clock,
         i_key   => tb_key,
         o_key_schedule => tb_key_schedule                 
     );
@@ -41,7 +45,14 @@ begin
     begin
         tb_key <= ((x"2b",x"7e",x"15",x"16"),(x"28",x"ae",x"d2",x"a6"),(x"ab",x"f7",x"15",x"88"),(x"09",x"cf",x"4f",x"3c"));
         wait for 1 ps;
+    end process;
     
+    clk_process :process
+    begin
+         tb_clock <= '0';
+         wait for clk_period/2;
+         tb_clock <= '1';
+         wait for clk_period/2;
     end process;
     
 
